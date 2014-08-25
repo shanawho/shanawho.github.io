4b21634c838ee7ee223956525804f97089db49ef
Shana Hu <shanawho@gmail.com>
2014-08-24 21:43:17 -0700

    iterated version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 (function() {
   var app;
-
   app = angular.module("app", ['ngRoute']);
 
   app.config(function($routeProvider) {
     return $routeProvider.when("/", {
       templateUrl: "pages/main.html",
-      controller: "mainController"
-    }).when("/work", {
+      controller: "mainController",
+      title: "Product Design & Frontend Development"
+    }).when("/main", {
       templateUrl: "pages/main.html",
       controller: "mainController",
       title: "Product Design & Frontend Development"
     }).when("/art", {
       templateUrl: "pages/art.html",
-      controller: "artController"
+      controller: "artController",
+      title: "Art"
     }).when("/about", {
       templateUrl: "pages/about.html",
       controller: "aboutController",
@@ -38,6 +39,10 @@
       templateUrl: "pages/32pt.html",
       controller: "32ptController",
       title: "32pt"
+    }).when("/lettering", {
+      templateUrl: "pages/lettering.html",
+      controller: "mainController",
+      title: "Lettering"
     }).otherwise({
       redirectTo: "/",
       title: "Product Design & Frontend Development"
@@ -51,29 +56,32 @@
       });
   }]);
 
+
   app.controller("mainController", function($scope) {
-  
+
     $scope.projects = [
       {title: "Confluence", path: "confluence", tagline: "Crowdsourced Q&A for foreign language learners"},
-      {title: "DaVinci Mobile", path: "davinci", tagline: "LinkedIn's mobile pattern library for designers and developers"},
+      {title: "DaVinci Mobile", path: "davinci", tagline: "LinkedIn's mobile pattern library for designers & developers"},
       {title: "Introduction to Illustrator & Photoshop", path: "decal", tagline: "A weekly 2-hour, 2-unit UC Berkeley DeCal course"},
-      {title: "32pt", path: "32pt", tagline: "Beautiful, affordable, custom letterpress business cards"},
-
+      {title: "32pt", path: "32pt", tagline: "A custom design editor for letterpressed business cards"},
+      {title: "Lettering", path: "lettering", tagline: "Illustrative typography"},
+      {title: "Artwork", path: "art", tagline: "Portraiture & paintings"}
     ]
 
   });
 
   app.controller("artController", function($scope) {
-    
+    $scope.params = "art";
   });
 
   app.controller("aboutController", function($scope) {
-    
+    $scope.params = "about";
+
   });
 
 
   app.controller("resumeController", function($scope) {
-    
+    $scope.params = "resume";
   });
 
   app.controller("davinciController", function($scope, $routeParams) {
@@ -87,15 +95,19 @@
   });
 
   app.controller("confluenceController", function($scope, $routeParams) {
-  
     $scope.params = "confluence";
-    console.log($scope.params);
 
   });
 
   app.controller("32ptController", function($scope, $routeParams) {
     $scope.params = "32pt";
   });
+
+
+  app.controller("letteringController", function($scope, $routeParams) {
+    $scope.params = "lettering";
+  });
+
 
 
 
