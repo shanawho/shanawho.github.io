31b4a7ac37c329bd67ba0e5dea6812b1396e6125
Shana Hu <shanawho@gmail.com>
2019-07-12 23:43:24 -0700

    Updates

---
+++ precache-manifest.6000fc720da0f899b9f20e36d279f909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07d29109ae50e70ba276f7c9fee4a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ccba6269fc2ee4d59c",
+    "url": "/static/css/main.9a5697bb.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "24ccba6269fc2ee4d59c",
+    "url": "/static/js/main.0167d06b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33cb7b16ba7e5d90f8bb275d8d0a378c",
+    "url": "/static/media/Lark-Bold_web.33cb7b16.woff"
+  },
+  {
+    "revision": "94d1dbab461f36099802a2dd130752e2",
+    "url": "/static/media/Lark-Light_web.94d1dbab.woff"
+  }
+]);
